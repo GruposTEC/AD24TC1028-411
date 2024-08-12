08cec4f034d6f8b7a1ad2ad1165b2dae69e6a11d
Antonio Mejia <58567224+avmejia-tec@users.noreply.github.com>
2024-08-12 14:45:00 +0000

    update

--- SEsion 2-1/variables.py
+++ SEsion 2-1/variables.py
@@ -4,4 +4,7 @@ x = float(input(" Dame un valor flotante:"))
 #x = float(x)
 print(type(x))
 y = math.ceil(x)
-print(y)
+print(" La variable y tiene como valor " + str(y))
+print(f'La variable y tiene como valor : {y+1}')
+#import keyword
+#print(keyword.kwlist)
