3ed6abcc5ce0844258063d36ae7ed8a1b537ef4a
Antonio Mejia <58567224+avmejia-tec@users.noreply.github.com>
2024-08-14 15:43:09 +0000

    decisiones

---
+++ 2-Decisiones/decisiones.py
@@ -0,0 +1,23 @@
+#inicio;
+m1 = int(input("Velor de m1 :"))#recibo m1;
+m2 = int(input("Velor de m2 :"))#recibo m2;
+m3 = int(input("Velor de m3 :"))#recibo m3;
+if m1 > m2:                     #if (m1 es mayor a m2)
+                                #{
+    x = m1                      #  x guarda el valor de m1;
+                                #}
+else:                           #else
+                                #{
+    x = m2                      #  x guarda el valor de m2;
+                                #}
+if m3 > x:                      #if (m3 es mayor a x)
+                                #{
+    z = m3                      #  z guarda el valor de m3;
+                                #}
+else:                           #else
+                                #{
+    z = x                       #  z guarda el valor de x;
+                                #}
+print(z)                        #regresabamos z;
+
+                                #fin;
---
+++ 2-Decisiones/prueba.py
@@ -0,0 +1,5 @@
+resultado = 0
+if 5 < 4:
+    resultado = "exito"
+else:
+    print(resultado)
