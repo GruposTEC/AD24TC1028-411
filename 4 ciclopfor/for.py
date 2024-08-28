c06c9b2a31533f5c51de63cf0cebedb9ce36e3b8
Antonio Mejia <58567224+avmejia-tec@users.noreply.github.com>
2024-08-28 13:50:31 +0000

    for

---
+++ 4 ciclopfor/for.py
@@ -0,0 +1,15 @@
+def main():
+    pass
+    for i in range(10):
+        print (i)
+
+    """
+    i=0
+    while(i<10):
+        print(i)
+        i+=1
+    """
+
+
+if __name__ == "__main__":
+    main()
---
+++ 4 ciclopfor/lecturaarchivo.py
@@ -0,0 +1,5 @@
+f = open("/workspaces/AD24TC1028-411/4 ciclopfor/archivo.txt")
+
+for linea in f:
+    print(linea)
+    print(type(linea))
